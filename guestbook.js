9455febfcc701508bf5b2bd1469c588a09e29b5f
ricardooddone <85978752+ricardooddone@users.noreply.github.com>
2022-10-27 10:08:18 -0300

    Update guetbook.js
    URL changes

--- guestbook.js
+++ guestbook.js
@@ -1,13 +1,13 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://us-south.functions.appdomain.cloud/api/v1/web/fbf2b3d1-46cb-4146-9a8a-16976fe0dafe/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
     return $.ajax({
       type: 'GET',
-      url: `${apiUrl}/entries`,
+      url: `${read-guestbook-entries-sequence}/entries`,
       dataType: 'json'
     });
   },
@@ -16,7 +16,7 @@ const guestbook = {
     console.log('Sending', name, email, comment)
     return $.ajax({
       type: 'PUT',
-      url: `${apiUrl}/entries`,
+      url: `${save-guestbook-entry-sequence}/entries`,
       contentType: 'application/json; charset=utf-8',
       data: JSON.stringify({
         name,
